6089e460c46b6114b485be3e268fbf5f0cb2adec
Xeaphii <muhammadzeeshan020@gmail.com>
2016-06-20 16:47:18 +0500

    Stil drafting

--- QLearn.py
+++ QLearn.py
@@ -1,6 +1,6 @@
 class QLearn:
 
-   def __init__(self, l_learning_rate = 0, l_discounting_factor = 0.5,l_actions):
+   def __init__(self,l_actions, l_learning_rate = 0, l_discounting_factor = 0.5):
       self.learning_rate = l_learning_rate
       self.discounting_factor = l_discounting_factor
       self.states = {}
@@ -13,9 +13,10 @@ class QLearn:
 
    def update_Q(self,l_state,l_action,l_reward,l_new_state):
       l_max = float("-inf")
-	  new_action = None
+      new_action = None
       for action in self.actions:
-            if l_max > get_Q_value(self,l_new_state,action):
-			      l_max =  get_Q_value(self,l_new_state,action)
-				  new_action = action
-      self.states[(l_state,l_action)] = (1-self.learning_rate)*self.states[(l_state,l_action)] +self.learning_rate*(self.discounting_factor *l_max)  
+            print action
+            if l_max > self.get_Q_value(l_new_state,action):
+                  l_max =  self.get_Q_value(l_new_state,action)
+                  new_action = action
+      self.states[(l_state,l_action)] = (1-self.learning_rate)*self.get_Q_value(l_state,l_action) +self.learning_rate*(self.discounting_factor *l_max)  
